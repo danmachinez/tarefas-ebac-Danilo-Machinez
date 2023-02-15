f94ff2523668d740046305facb79e8a3646c4ccc
Danilo Machinez <dmcdev@proton.me>
2023-02-15 16:14:44 -0300

    commit inicial

---
+++ tarefa-calculo-media/Medias/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        mediaAritmetica();
+    }
+
+    private static void mediaAritmetica() {
+
+        int nota1 = 5;
+        int nota2 = 8;
+        int nota3 = 9;
+        int nota4 = 4;
+
+        double resultadoMedia = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println(resultadoMedia);
+
+    }
+}
