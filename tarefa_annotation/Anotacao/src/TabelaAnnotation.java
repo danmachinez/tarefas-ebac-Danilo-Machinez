e364480354e9e97abe2254d2034b7f4029120288
Danilo Machinez <dmcdev@proton.me>
2023-05-15 16:43:23 -0300

    commit inicial

---
+++ tarefa_annotation/Anotacao/src/TabelaAnnotation.java
@@ -0,0 +1,6 @@
+@Tabela(nome = "TabelaAnnotation")
+public class TabelaAnnotation {
+    public static void main(String[] args) {
+        System.out.println("Finge que isso é uma tabela kkkk");
+    }
+}
