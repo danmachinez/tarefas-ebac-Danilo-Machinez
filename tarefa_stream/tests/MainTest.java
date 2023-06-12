33d251130ae66338c7c6f91e1363ec1f6d0807d2
Danilo Machinez <dmcdev@proton.me>
2023-06-12 17:02:22 -0300

    adicionando teste a classe Main

--- tarefa_stream/tests/MainTest.java
+++ tarefa_stream/tests/MainTest.java
@@ -8,20 +8,18 @@ public class MainTest {
 
     @Test
     public void testListaNomesFemininos() {
-        // Arrange
+
         List<String> listaNomesGeneros = new ArrayList<>();
         listaNomesGeneros.add("Ana-Feminino");
         listaNomesGeneros.add("Carlos-Masculino");
         listaNomesGeneros.add("Maria-Feminino");
         listaNomesGeneros.add("João-Masculino");
 
-        // Act
         List<String> listaNomesFemininos = listaNomesGeneros.stream()
                 .filter(nomeGenero -> nomeGenero.split("-")[1].equalsIgnoreCase("Feminino"))
                 .map(nomeGenero -> nomeGenero.split("-")[0])
                 .toList();
 
-        // Assert
         Assertions.assertTrue(listaNomesFemininos.contains("Ana"));
         Assertions.assertTrue(listaNomesFemininos.contains("Maria"));
         Assertions.assertFalse(listaNomesFemininos.contains("Carlos"));
